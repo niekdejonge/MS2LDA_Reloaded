ea57ef562d4cfa027daee9dfa1cdf25849ed199d
Jonge <niek.dejonge@wur.nl>
2022-08-30 18:40:47 +0200

    Use Fragments for mass2motif

--- code/automatically_annotate_mass2motifs/mass2motif.py
+++ code/automatically_annotate_mass2motifs/mass2motif.py
@@ -3,6 +3,8 @@ import numpy as np
 from math import gcd
 from functools import reduce
 from matchms import Fragments
+import re
+
 
 class Mass2Motif:
     """Stores Mass2Motif information
@@ -11,26 +13,43 @@ class Mass2Motif:
 
     def __init__(self, words: List[str],
                  probabilities: List[float],
-                 motif_name):
-        assert words
-        self.words = words
-        self.probabilities = probabilities
+                 motif_name: str = None):
+        self.assert_correct_input(words, probabilities)
         self.motif_name = motif_name
-        fragments = Fragments(np.array(list(binned_masses.keys())), np.array(list(binned_masses.values())))
 
-        self.bin_size = self.get_bin_size()
-        self.assert_correct_mass2motif()
+        peaks, peak_probabilities, losses, loss_probabilities = self.convert_words_to_peaks(words, probabilities)
+        self.fragments = Fragments(np.array(peaks), np.array(peak_probabilities))
+        self.losses = Fragments(np.array(losses), np.array(loss_probabilities))
 
-    def __str__(self):
-        return str({"motif_name": self.motif_name, "words": self.words, "probabilities": self.probabilities})
+        self.bin_size = self.get_bin_size(words)
 
-    def assert_correct_mass2motif(self):
-        """Checks if words and probabilities are in the expected format"""
-        assert isinstance(self.words, list)
-        assert isinstance(self.probabilities, list)
-        assert len(self.words) > 0, "More than 0 words are expected in a mass2motif"
-        assert len(self.words) == len(self.probabilities), "An equal number of words and probabilities was expected"
-        for word in self.words:
+    @staticmethod
+    def convert_words_to_peaks(words, probabilities):
+        peaks = []
+        peak_probabilities = []
+        losses = []
+        loss_probabilities = []
+        for i, word in enumerate(words):
+            result = re.search("\A(fragment|loss)_([0-9]+\.[0-9]+)\Z", word)
+            is_loss = result.group(1) == "loss"
+            fragment_mz = float(result.group(2))
+            if not is_loss:
+                peaks.append(fragment_mz)
+                peak_probabilities.append(probabilities[i])
+            else:
+                losses.append(fragment_mz)
+                loss_probabilities.append((probabilities[i]))
+        return peaks, peak_probabilities, losses, loss_probabilities
+
+    @staticmethod
+    def assert_correct_input(words, probabilities):
+        """Checks if words and probabilities are in the expected format
+        """
+        assert isinstance(words, list)
+        assert isinstance(probabilities, list)
+        assert len(words) > 0, "More than 0 words are expected in a mass2motif"
+        assert len(words) == len(probabilities), "An equal number of words and probabilities was expected"
+        for word in words:
             assert isinstance(word, str), "A list of strings was expected as words"
             word_and_fragment = word.split("_")
             assert len(word_and_fragment) == 2, "Words should contain 1 underscore"
@@ -43,13 +62,14 @@ class Mass2Motif:
             decimal = whole_number_and_decimal[1]
             assert whole_number.isdigit(), "An unexpected word format was given"
             assert decimal.isdigit(), "An unexpected word format was given"
-        for probability in self.probabilities:
+        for probability in probabilities:
             assert isinstance(probability, float)
 
-    def get_bin_size(self):
+    @staticmethod
+    def get_bin_size(words):
         """Determines the bin_size from the fragment sizes"""
         list_of_decimals = []
-        for word in self.words:
+        for word in words:
             decimals_str = word.split(".", 1)[1]
             decimals = int(decimals_str)
             list_of_decimals.append(decimals)
@@ -58,3 +78,18 @@ class Mass2Motif:
         greatest_common_denominator = reduce(gcd, list_of_decimals)
         bin_size = greatest_common_denominator/(10**nr_of_decimals)*2
         return bin_size
+
+    def __str__(self):
+        return str({"motif_name": self.motif_name,
+                    "fragments": self.fragments.mz,
+                    "fragment_probabilities": self.fragments.intensities,
+                    "losses": self.losses.mz,
+                    "loss_probabilities": self.losses.intensities})
+
+
+
+if __name__ == "__main__":
+    mass2motif = Mass2Motif(['fragment_375.2225', 'loss_80.0275', 'loss_128.0625', 'loss_141.1025', 'loss_147.0525'],
+                            [0.005584277434409811, 0.0012797822397661643, 0.0019930865131162247, 0.0026998426721283244, 0.0032813585667783463],
+                            'Urine_MM_motif_319.m2m')
+    print(mass2motif)
