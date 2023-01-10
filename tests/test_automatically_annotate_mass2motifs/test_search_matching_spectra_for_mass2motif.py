f18a30c27b9b0882e4fd1460a294e4e6395cfab9
Jonge <niek.dejonge@wur.nl>
2023-01-10 09:52:07 +0100

    changed some function names and linting

--- tests/test_automatically_annotate_mass2motifs/test_search_matching_spectra_for_mass2motif.py
+++ tests/test_automatically_annotate_mass2motifs/test_search_matching_spectra_for_mass2motif.py
@@ -7,6 +7,7 @@ from automatically_annotate_mass2motifs.search_matching_spectra_for_mass2motif i
 from automatically_annotate_mass2motifs.mass2motif import Mass2Motif
 from tests.test_automatically_annotate_mass2motifs.generate_test_data import binned_spectra_005
 
+
 def test_overlap_in_fragments():
     spectrum_fragment = Fragments(np.array([12.3, 14.5, 23.4, 26.7]), np.array([1.0, 0.2, 0.5, 0.3]))
     mass2motif_fragment = Fragments(np.array([12.3, 18.5, 26.7]), np.array([1.0, 0.2, 0.3]))
@@ -14,6 +15,7 @@ def test_overlap_in_fragments():
     result = overlap_in_fragments(mass2motif_fragment, spectrum_fragment)
     assert np.all(result.mz == np.array([12.3, 18.5, 26.7]))
     assert np.all(result.intensities == np.array([1.0, 0, 0.3]))
+
 
 def test_similarity_mass2motif_and_spectrum():
     binned_spectrum = Spectrum(mz=np.array([100.0025, 200.0025], dtype="float"),
@@ -45,14 +47,11 @@ def test_select_spectra_matching_mass2motif(tmp_path):
     spectra_per_mass2motif = spectra_containing_mass2motif.select_matching_spectra(0.05)
     expected_spectra_per_mass2motif = [[spectra[0], spectra[1]], [spectra[1]]]
     assert np.all(spectra_per_mass2motif == expected_spectra_per_mass2motif), "Different spectra were expected"
-    smiles_matching, smiles_not_matching = spectra_containing_mass2motif.select_non_matching_smiles(
+    smiles_matching, smiles_not_matching = spectra_containing_mass2motif.select_unique_matching_and_non_matching_smiles(
         spectra_per_mass2motif[0])
     assert set(smiles_matching) == {'CN=C=O', 'C1CCCCC1'}
     assert smiles_not_matching == []
-    smiles_matching, smiles_not_matching = spectra_containing_mass2motif.select_non_matching_smiles(
+    smiles_matching, smiles_not_matching = spectra_containing_mass2motif.select_unique_matching_and_non_matching_smiles(
         spectra_per_mass2motif[1])
     assert smiles_matching == ["C1CCCCC1"]
     assert smiles_not_matching == ["CN=C=O"]
-
-if __name__ == "__main__":
-    pass
