a8ec925dd69325236c98c995c837230ff4278cee
Jonge <niek.dejonge@wur.nl>
2023-01-18 15:47:57 +0100

    Added test save mass2motif

--- tests/test_automatically_annotate_mass2motifs/test_mass2motif.py
+++ tests/test_automatically_annotate_mass2motifs/test_mass2motif.py
@@ -1,6 +1,12 @@
+import os
 import numpy as np
-from automatically_annotate_mass2motifs.mass2motif import Mass2Motif
+from automatically_annotate_mass2motifs.mass2motif import Mass2Motif, load_mass2motifs_json
+from automatically_annotate_mass2motifs.utils import store_as_json
 from automatically_annotate_mass2motifs.download_mass2motifs import convert_words_to_peaks
+from automatically_annotate_mass2motifs.annotation import Annotation
+from automatically_annotate_mass2motifs.moss_annotation import load_moss_results
+from tests.test_automatically_annotate_mass2motifs.generate_test_data import generate_moss_results_file
+
 
 def test_convert_words_to_peaks():
     words = ['fragment_375.2225',
@@ -43,18 +49,35 @@ def test_creating_mass2motif_with_correct_mass_bin():
 def test_creating_mass2motif_with_incorrect_mass_bin():
     error_raised = False
     try:
-        mass2motif = Mass2Motif(fragments=[375.2225], fragment_probabilities=[0.0055], losses=[80.0275, 128.0625],
-                                loss_probabilities=[0.0012, 0.0019], bin_size=0.5)
+        Mass2Motif(fragments=[375.2225], fragment_probabilities=[0.0055], losses=[80.0275, 128.0625],
+                   loss_probabilities=[0.0012, 0.0019], bin_size=0.5)
     except AssertionError:
         error_raised = True
     assert error_raised, "An assert should warn for a wrongly specified bin_size"
 
-def test_download_mass2motifs():
-    pass
+def test_save_and_load_mass2motif_as_json(tmp_path):
+    file_name = os.path.join(tmp_path, "moss_results")
+    generate_moss_results_file(file_name)
+    moss_annotations = load_moss_results(file_name)
+    annotation = Annotation(moss_annotations=moss_annotations,
+                            minimal_similarity=0.3,
+                            moss_minimal_relative_support=40.0,
+                            moss_maximal_relative_support_complement=70.0,
+                            nr_of_spectra_matching_mass2motif=10,
+                            nr_of_spectra_not_matching_mass2motif=100)
 
-def test_get_annotation():
-    pass
+    mass2motifs = [Mass2Motif(fragments=[100.025], fragment_probabilities= [0.5],
+                            losses=[128.075, 200.725], loss_probabilities=[0.1, 0.25],
+                            bin_size=0.05, moss_annotations=[annotation]),
+                   Mass2Motif(fragments=[50.025], fragment_probabilities=[0.6],
+                              losses=[80.025, 128.075], loss_probabilities=[0.5, 0.1],
+                              bin_size=0.05)]
 
+    file_name = os.path.join(tmp_path, "mass2motifs.json")
+    store_as_json(mass2motifs, file_name)
+    loaded_mass2motifs = load_mass2motifs_json(file_name)
+    for i, mass2motif in enumerate(mass2motifs):
+        mass2motif.assert_equal(loaded_mass2motifs[i])
 
 if __name__ == "__main__":
     pass
