95a74b4a9cef714025999c7dd2aa046db324f2a0
Jonge <niek.dejonge@wur.nl>
2023-01-25 11:55:48 +0100

    Clean up and complete main pipeline

--- automatically_annotate_mass2motifs/main.py
+++ automatically_annotate_mass2motifs/main.py
@@ -1,66 +1,75 @@
 import os
+from typing import Tuple
 
 from automatically_annotate_mass2motifs.scores_matrix import ScoresMatrix
-from automatically_annotate_mass2motifs.utils import load_pickled_file
+from automatically_annotate_mass2motifs.utils import load_pickled_file, store_as_json, store_pickled_file
 from automatically_annotate_mass2motifs.moss_annotation import get_multiple_moss_annotations
 from automatically_annotate_mass2motifs.mass2motif import load_mass2motifs_json
+from automatically_annotate_mass2motifs.download_mass2motifs import download_motif_set_from_motifdb
+from automatically_annotate_mass2motifs.clean_library_spectra import get_cleaned_and_binned_spectra
+from automatically_annotate_mass2motifs.scores_matrix import create_similarity_matrix
 
 
-def pipeline_from_scratch(raw_library_spectra_file,
-                          motif_set_name,
-                          directory_to_store_in_between_steps):
-    pass
-    # # Process library_spectra
-    #
-    # # Save the in between processed spectra.
-    # # Save the binned spectra
-    # binned_and_filtered_library_spectra =
-    # # Load Mass2motifs
-    # # Store the Mass2motifs as json
-    # mass2motifs =
-    #
-    # # Create a similarity matrix
-    # # Store the similarity matrix as pickled file
-    # scores_matrix = create_similarity_matrix(mass2motifs,
-    #                                          binned_and_filtered_library_spectra)
-    # spectra_selector = ScoresMatrix(binned_and_filtered_library_spectra, scores_matrix)
-    # store_pickled_file(spectra_selector.scores_matrix, scores_matrix_file)
-    #
-    # # Add annotations to mass2motifs
-    # # Replace previously stored json with mass2motifs, with mass2motifs containing annotations.
-    #
-    # # Store visualizations of results
+def main(raw_library_spectra_file: str,
+         motif_set_name: str,
+         bin_size: float,
+         ion_mode: str,
+         directory_to_store_in_between_steps: str,
+         similarity_thresholds: Tuple[float, ...],
+         load_previous_annotation=False):
+    """Pipeline for adding annotations to mass2motifs and storing in between files"""
+    mass2motifs_file_name = os.path.join(directory_to_store_in_between_steps,
+                                         f"mass2motifs_{motif_set_name}.json")
+    annotated_mass2motifs_file_name = os.path.join(directory_to_store_in_between_steps,
+                                                   f"annotated_mass2motifs_{motif_set_name}.json")
+    if load_previous_annotation:
+        assert os.path.isfile(annotated_mass2motifs_file_name), \
+            "Annotated file does not yet exist, set load_previous_annotation to False"
+        mass2motifs = load_mass2motifs_json(annotated_mass2motifs_file_name)
+    elif os.path.isfile(mass2motifs_file_name):
+        print("loading mass2motifs")
+        mass2motifs = load_mass2motifs_json(mass2motifs_file_name)
+    else:
+        # Download mass2motifs from ms2lda.org and storing it
+        mass2motifs = download_motif_set_from_motifdb(motif_set_name, bin_size)
+        store_as_json(mass2motifs,
+                      mass2motifs_file_name)
+        print(f"mass2motifs are stored in: {mass2motifs_file_name}")
 
+    spectra = get_cleaned_and_binned_spectra(raw_library_spectra_file,
+                                             bin_size,
+                                             ion_mode)
 
-def pipeline_from_in_between_results(mass2motif_file,
-                                     binned_spectra_file,
-                                     scores_matrix_file,
-                                     similarity_thresholds,
-                                     results_file_name):
-    print("loading mass2motifs")
-    mass2motifs = load_mass2motifs_json(mass2motif_file)
-    print("Loading precalculated scores matrix and spectra")
-    scores_matrix = ScoresMatrix(load_pickled_file(binned_spectra_file),
-                                 load_pickled_file(scores_matrix_file))
+    scores_matrix_file_name = os.path.join(directory_to_store_in_between_steps,
+                                           f"scores_matrix_{motif_set_name}.pickle")
+    if os.path.isfile(mass2motifs_file_name):
+        scores_matrix = load_pickled_file(scores_matrix_file_name)
+        print(f"scores matrix is stored in: {scores_matrix_file_name}")
+    else:
+        print("Loading precalculated scores matrix and spectra")
+        scores_matrix = create_similarity_matrix(mass2motifs,
+                                                 spectra)
+        store_pickled_file(scores_matrix, scores_matrix_file_name)
+
+    spectra_selector = ScoresMatrix(spectra, scores_matrix)
+
+    # Saves the annotations of the mass2motifs in the json containing the mass2motifs.
     mass2motifs = get_multiple_moss_annotations(mass2motifs, scores_matrix,
                                                 similarity_thresholds,
-                                                60,
-                                                80,
-                                                results_file_name)
+                                                minimal_relative_support=60,
+                                                maximal_relative_support_complement=80,
+                                                save_in_between_file=annotated_mass2motifs_file_name)
+    return spectra_selector, mass2motifs
+
 
 if __name__ == "__main__":
-    base_directory = "../data/automatic_annotation/gnps_library_derived_mass2motifs/scores_matrix/"
+    directory_to_store_in_between_steps = "../data/automatic_annotation/gnps_library_derived_mass2motifs/"
 
-    pipeline_from_in_between_results(os.path.join(base_directory, "all_annotated_mass2motifs_gnps_library_derived_mass2motifs.json"),
-                                     os.path.join(base_directory, "../../library_spectra/all_gnps_positive_ionmode_cleaned_filtered_binned_0.005.pickle"),
-                                     os.path.join(base_directory, "mass2motif_scores_matrix_GNPS library derived Mass2Motifs(1).pickle"),
-                                     [0.1, 0.3, 0.6, 0.8, 0.9],
-                                     os.path.join(base_directory, "all_annotated_mass2motifs_gnps_library_derived_mass2motifs.json"))
-    # annotated_mass2motifs = load_mass2motifs_json(os.path.join(base_directory, "annotated_mass2motifs_gnps_library_derived_mass2motifs.json"))
-    # for mass2motif in annotated_mass2motifs:
-    #     plt.Figure()
-    #     fig = plot_mass2motif(mass2motif)
-    #     fig.show()
-    #     for annotation in mass2motif.moss_annotations:
-    #         fig = annotation.visualize(6)
-    #         fig.show()
+    main(raw_library_spectra_file=os.path.join(directory_to_store_in_between_steps,
+                                               "../library_spectra/all_gnps.mgf"),
+         motif_set_name="gnps library derived mass2motifs",
+         bin_size=0.005,
+         ion_mode="positive",
+         directory_to_store_in_between_steps=directory_to_store_in_between_steps,
+         similarity_thresholds=(0.1, 0.3, 0.6, 0.8, 0.9,),
+         load_previous_annotation=True)
